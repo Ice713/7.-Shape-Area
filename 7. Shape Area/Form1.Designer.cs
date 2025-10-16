7cec619cc6efb42c3de2713e5df42a0a0186c93d
Ice713 <icramzilruiz@gmail.com>
2025-10-16 16:30:33 +0800

    Done with UI. Almost done with Shape and Rectangle Classes. Working with event handlers.

--- 7. Shape Area/Form1.Designer.cs
+++ 7. Shape Area/Form1.Designer.cs
@@ -31,6 +31,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownLength = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -46,31 +52,82 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(211, 101);
+            this.label2.Location = new System.Drawing.Point(297, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(50, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Length:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(211, 131);
+            this.label3.Location = new System.Drawing.Point(297, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 16);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Width:";
+            // 
+            // numericUpDownLength
+            // 
+            this.numericUpDownLength.DecimalPlaces = 5;
+            this.numericUpDownLength.Location = new System.Drawing.Point(377, 97);
+            this.numericUpDownLength.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownLength.Name = "numericUpDownLength";
+            this.numericUpDownLength.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownLength.TabIndex = 3;
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.DecimalPlaces = 5;
+            this.numericUpDownWidth.Location = new System.Drawing.Point(377, 127);
+            this.numericUpDownWidth.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownWidth.TabIndex = 4;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(332, 187);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(147, 44);
+            this.buttonCalculate.TabIndex = 5;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(358, 271);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(48, 16);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "Result:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.numericUpDownWidth);
+            this.Controls.Add(this.numericUpDownLength);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "7. Shape Area";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +138,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownLength;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
---
+++ 7. Shape Area/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Shape_Area
+{
+    internal class Shape
+    {
+        public virtual double Area()
+        {
+            return 0;
+        }
+
+        public Shape() { }
+    }
+}
